4e3fb38da99f0846dcebebea6b840d876c94d8b7
noobylub <73588764+noobylub@users.noreply.github.com>
2021-12-20 21:02:35 +0300

    new report card

--- src/App.js
+++ src/App.js
@@ -247,7 +247,7 @@ function App() {
           <a href="https://drive.google.com/file/d/1PM6IN55BVnCMazCI_ge1G1p9tAcNI3WD/view?usp=sharing">
             <h1 className="year">Junior</h1>
           </a>
-          <a href="https://drive.google.com/file/d/10GzBnfuLPmDgeDThwTlB8mpb_-3roDMu/view?usp=sharing">
+          <a href="https://drive.google.com/file/d/1eFm58wYcGm1rkEM_XJWyIWO1tdHHe0iB/view?usp=sharing">
             <h1 className="year">Senior</h1>
           </a>
         </div>
